30c9655408ebc0a9a34be303d60e6e42acf2b429
JunYoung Lee <82147229+soft-jun@users.noreply.github.com>
2022-04-29 14:44:24 +0900

    Add files via upload

---
+++ 자기소개.java
@@ -0,0 +1,9 @@
+
+public class 자기소개 {
+
+	public static void main (String[] args) {
+		System.out.println("자기소개");
+		System.out.println("수원정보과학고등학교 소프트웨어과 2학년 1반");
+		System.out.println("취미는 운동하기 게임하기 코딩하기");
+	}
+}
